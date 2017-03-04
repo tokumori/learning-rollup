ab72a8ce62a7b9a220cbf90e7c1f170c6f8f2178
Bryce <bsaito88@gmail.com>
2017-03-04 00:05:54 -1000

    Added watcher and livereload
    - Installed rollup-watch to watch for changes in src.
    - Installed livereload to watch for changes in public.
    - Injected livereload script in Javascript when the ENV variable is not production.
    - Installed npm-run-all to run rollup-watch and livereload parallel.

--- src/app.js
+++ src/app.js
@@ -14,6 +14,12 @@ if (ENV !== 'production') {
   // Enable logger.
   debug.enable('*');
   log('Logging is enabled!');
+
+  // Enable LiveReload
+  document.write(
+    '<script src="http://' + (location.host || 'localhost').split(':')[0] +
+    ':35729/livereload.js?snipver=1"></' + 'script>'
+  );
 } else {
   debug.disable();
 }
