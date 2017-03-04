07ec56502cbaa45da61704b90d122483d471fbce
Bryce <bsaito88@gmail.com>
2017-03-03 14:39:58 -1000

    Installed babel and added to config file.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,13 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
   entry: 'src/app.js',
   dest: 'public/js/app.js',
   format: 'iife',
   sourceMap: 'inline',
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
 };
