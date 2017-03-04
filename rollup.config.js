a3f0bda65f32ff1888fb479b7b6afd714b649d50
Bryce <bsaito88@gmail.com>
2017-03-03 23:41:12 -1000

    Added more PostCSS plugins.
    - Added simple-vars for SASS-like variables
    - Added nested for SASS-like nesting
    - Added cssnext for older browser compatibility
    - Added cssnano to uglify css.

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,10 @@ import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
 import postcss from 'rollup-plugin-postcss';
+import simplevars from 'postcss-simple-vars';
+import nested from 'postcss-nested';
+import cssnext from 'postcss-cssnext';
+import cssnano from 'cssnano';
 
 export default {
   entry: 'src/app.js',
@@ -13,6 +17,12 @@ export default {
   sourceMap: 'inline',
   plugins: [
     postcss({
+      plugins: [
+        simplevars(),
+        nested(),
+        cssnext({warnForDuplicates: false}),
+        cssnano(),
+      ],
       extensions: ['.css'],
     }),
     resolve({
