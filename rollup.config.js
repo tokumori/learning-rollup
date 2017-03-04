d3f59b63f73f96c2e5cbc5824fbb65c77d70c76e
Bryce <bsaito88@gmail.com>
2017-03-03 14:56:56 -1000

    Added eslint, commonjs, resolve plugins.

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
 import babel from 'rollup-plugin-babel';
+import eslint from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   entry: 'src/app.js',
@@ -6,6 +9,17 @@ export default {
   format: 'iife',
   sourceMap: 'inline',
   plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+    eslint({
+      exclude: [
+        'src/styles/**',
+      ]
+    }),
     babel({
       exclude: 'node_modules/**',
     }),
